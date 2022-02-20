e9968fe326e6b3e16511bac9dce72b5ea098a85c
Dany <danymike.joseph@gmail.com>
2022-02-20 13:33:55 +0100

    feat: map initialisation with marker and geolocation using hard valiue

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,13 +1,25 @@
 import * as React from 'react';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import { StyleSheet, Dimensions } from 'react-native';
 import * as Styled from './Map.styles'
 
 
 export const Map: React.FunctionComponent = ({}) => {
+  let location = {
+    latitude: 23.259933,
+    longitude: 77.412613,
+    latitudeDelta: 0.09,
+    longitudeDelta: 0.09,
+  }
+
   return (
     <Styled.MapContainer>
-      <MapView style={styles.map} />
+      <MapView 
+        style={styles.map}
+        region={location}
+      >
+        <Marker coordinate={{ latitude: 23.259933, longitude: 77.412613}} />
+      </MapView>
     </Styled.MapContainer>
   );
 }
