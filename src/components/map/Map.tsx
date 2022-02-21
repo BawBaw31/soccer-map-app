2c35f663da954864afbcabf41efb68da11052910
Dany <danymike.joseph@gmail.com>
2022-02-21 08:33:47 +0100

    feat: fetch geolocation data done

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,33 +1,60 @@
-import * as React from 'react';
-import MapView, { Marker } from 'react-native-maps';
-import { StyleSheet, Dimensions } from 'react-native';
-import * as Styled from './Map.styles'
-
+import React, { useState, useEffect } from "react";
+import MapView, { Marker } from "react-native-maps";
+import { Text, View } from 'react-native'
+import { StyleSheet, Dimensions } from "react-native";
+import * as Location from "expo-location";
+import * as Styled from "./Map.styles";
 
 export const Map: React.FunctionComponent = ({}) => {
-  let location = {
+  const [location, setLocation]: any = useState(null);
+  const [errorMsg, setErrorMsg]: any = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+
+  let geolocation = {
     latitude: 23.259933,
     longitude: 77.412613,
     latitudeDelta: 0.09,
     longitudeDelta: 0.09,
+  };
+
+  let text: any = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
   }
 
   return (
-    <Styled.MapContainer>
-      <MapView 
-        style={styles.map}
-        region={location}
-      >
-        <Marker coordinate={{ latitude: 23.259933, longitude: 77.412613}} />
-      </MapView>
-    </Styled.MapContainer>
+    <View>
+      <View>
+        <Text>{text}</Text>
+      </View>
+      <Styled.MapContainer>
+        <MapView style={styles.map} region={geolocation}>
+          <Marker coordinate={{ latitude: 23.259933, longitude: 77.412613 }} />
+        </MapView>
+      </Styled.MapContainer>
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   map: {
     flex: 1,
-    width: Dimensions.get('window').width / 1.1,
-    height: Dimensions.get('window').height,
+    width: Dimensions.get("window").width / 1.1,
+    height: Dimensions.get("window").height,
   },
 });
