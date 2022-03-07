e2fd5b3347229b0ef3ebbd03e12b404dec944929
Dany <danymike.joseph@gmail.com>
2022-03-07 16:11:16 +0100

    react native snap carousel installed

--- src/components/games/Games.tsx
+++ src/components/games/Games.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import { Text } from 'react-native'
 
-export const Games: React.FunctionComponent = ({}) => {
-
-  return (
-    <Text>Games text</Text>
-  )
+export const Games: React.FunctionComponent = () => {
+    return <Text>Games text</Text>
 }
