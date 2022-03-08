5b1571cb1a2c8847dffaf30a2766c163bd43f7d4
Dany <danymike.joseph@gmail.com>
2022-03-08 18:29:24 +0100

    Button.tsx component created wip

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import * as Styled from './Button.styles'
+
+export const Button: React.FunctionComponent = () => {
+    return <Styled.ButtonContainer>Add your stadium</Styled.ButtonContainer>
+}
