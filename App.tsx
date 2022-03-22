83554de56ba6072ab59a47a49c935121910a3057
Arthur Isnard <artisnard@gmail.com>
2022-03-22 16:41:06 +0100

    refacto: prettier syntax

--- App.tsx
+++ App.tsx
@@ -1,33 +1,32 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { NavigationContainer } from '@react-navigation/native'
-import AppLoading from 'expo-app-loading';
+import AppLoading from 'expo-app-loading'
 import { RootNavigator } from './src/navigation/RootNavigator'
 import {
-  useFonts,
-  Montserrat_400Regular,
-  Montserrat_500Medium,
-  Montserrat_600SemiBold,
-  Montserrat_700Bold,
-} from '@expo-google-fonts/montserrat'
-
-export default function App() {
-
-  let [fontsLoaded] = useFonts({
+    useFonts,
     Montserrat_400Regular,
     Montserrat_500Medium,
     Montserrat_600SemiBold,
     Montserrat_700Bold,
-  })
+} from '@expo-google-fonts/montserrat'
 
-  if (!fontsLoaded) {
-    return <AppLoading />
-  } else {
-    return (
-        <NavigationContainer>
-          <StatusBar style='auto' />
-          <RootNavigator />
-        </NavigationContainer>
-    )
-  }
+export default function App() {
+    const [fontsLoaded] = useFonts({
+        Montserrat_400Regular,
+        Montserrat_500Medium,
+        Montserrat_600SemiBold,
+        Montserrat_700Bold,
+    })
+
+    if (!fontsLoaded) {
+        return <AppLoading />
+    } else {
+        return (
+            <NavigationContainer>
+                <StatusBar style="auto" />
+                <RootNavigator />
+            </NavigationContainer>
+        )
+    }
 }
