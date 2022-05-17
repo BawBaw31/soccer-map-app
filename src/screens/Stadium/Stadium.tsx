9144a89944f007d256f52f68a7784e9ef8de6e6e
Dany <danymike.joseph@gmail.com>
2022-05-17 21:23:02 +0200

    feat: remove get games call

--- src/screens/Stadium/Stadium.tsx
+++ src/screens/Stadium/Stadium.tsx
@@ -1,9 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ScrollView } from 'react-native'
 import { TitleLayout } from '../../components/layouts/Layouts'
-import { onValue, ref } from 'firebase/database'
-import { db } from '../../firebase/firebase-setup'
-import { useEffect, useState } from 'react'
 import { Games } from '../../components/games/GamesCarousel'
 interface StadiumProps {
     route: any
@@ -14,21 +11,16 @@ export const Stadium = (props: StadiumProps) => {
 
     useEffect(() => {
         const ac = new AbortController()
-        try {
-            onValue(ref(db, `stadiums/${props.route.params.stadium.id}/games`), (snapshot) => {
-                setGames([])
-                const data = snapshot.val()
-                if (data !== null) {
-                    Object.values(data).map((game: any) => {
-                        setGames((oldGames: any) => [...oldGames, game])
-                    })
-                }
+        setGames([])
+        const data = props.route.params.stadium.games
+        if (data !== null) {
+            Object.values(data).map((game: any) => {
+                setGames((oldGames: any) => [...oldGames, game])
             })
-        } catch (e) {
-            console.log('Error on getting data : ' + e)
         }
         return () => ac.abort()
     }, [])
+
     return (
         <TitleLayout title="Stadium screen">
             <ScrollView>
