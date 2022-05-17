83e7d49b6ed7e84d0ff622c520c7b2afb53c2557
Dany <danymike.joseph@gmail.com>
2022-05-17 22:58:39 +0200

    feat: game page wip

--- src/screens/Game/Game.tsx
+++ src/screens/Game/Game.tsx
@@ -1,15 +1,36 @@
 import React from 'react'
 import { ScrollView } from 'react-native'
 import { TitleLayout } from '../../components/layouts/Layouts'
+import * as Styled from './Game.styles'
+import { onValue, ref } from 'firebase/database'
+import { db } from '../../firebase/firebase-setup'
+import { useEffect, useState } from 'react'
 
 interface GameProps {
     route: any
 }
 
 export const Game = (props: GameProps) => {
+    const [game, setGame] = useState<any>({})
+
+    useEffect(() => {
+        const ac = new AbortController()
+        try {
+            onValue(ref(db, `games/${props.route.params.game.id}`), (snapshot) => {
+                const data = snapshot.val()
+                setGame(data)
+            })
+        } catch (e) {
+            console.log('Error on getting data : ' + e)
+        }
+        return () => ac.abort()
+    }, [])
+
     return (
         <TitleLayout title={props.route.params.game.name}>
-            <ScrollView></ScrollView>
+            <ScrollView>
+                <Styled.GameStadium>Stadium : {game.stadium?.title}</Styled.GameStadium>
+            </ScrollView>
         </TitleLayout>
     )
 }
