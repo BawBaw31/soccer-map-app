eb18f67ed297560eb6a3d248530b583ae6aa291f
Dany <danymike.joseph@gmail.com>
2022-05-24 01:42:59 +0200

    feat: add validation on New game page

--- src/components/formField/FormField.tsx
+++ src/components/formField/FormField.tsx
@@ -5,12 +5,13 @@ interface CustomButtonProps {
     text: string
     onPress(): void
     submit?: boolean
+    isDisabled?: boolean
 }
 
 export const CustomButton = (props: CustomButtonProps) => {
     return (
         <Styled.SubmitButtonContainer>
-            <Styled.SubmitButton onPress={props.onPress}>
+            <Styled.SubmitButton onPress={props.onPress} disabled={props.isDisabled}>
                 <Styled.SubmitButtonLabel>{props.text}</Styled.SubmitButtonLabel>
             </Styled.SubmitButton>
         </Styled.SubmitButtonContainer>
